c760a7d392148bd1812a7a2f94dc7af25e19a809
backspacev9 <backspacev9@gmail.com>
2021-07-21 01:54:52 +0300

    HerokuDBFix

--- src/App.ts
+++ src/App.ts
@@ -3,7 +3,9 @@ import * as dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 dotenv.config({ path: __dirname + "/.env" });
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient(
+  "mongodb+srv://user:testUser@cluster0.spcqv.mongodb.net/EFKDB"
+);
 
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
